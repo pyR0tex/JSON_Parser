dccd2f464216548c565686cf56eb4ef700fe72f1
Ro <rohit6cheruku@gmail.com>
2024-07-01 20:18:01 -0700

    First Commit with blank python file

---
+++ Parser.py
@@ -0,0 +1,16 @@
+'''
+Logic for parsing JSON files
+'''
+class Parser:
+
+    def _parser(self):
+        print("JSON Parser")
+        
+
+
+def Main():
+    parser = Parser()
+    parser._parser()
+
+if __name__ == '__main__':
+    Main()
