c37d4fbd3548aff8569fef3cddd025fefc37e0af
Ro <rohit6cheruku@gmail.com>
2024-07-12 16:27:53 -0700

    Starting to understand the logic

--- jsonParser.py
+++ jsonParser.py
@@ -30,7 +30,7 @@ class JSON_Parser:
         self.skip_whitespace()
 
         if self.jsonStr[self.index] not in ["{", "["]:
-            raise JSON_Exception('a valid JSON payload should be an object or array\n')
+            raise JSON_Exception('a valid JSON payload should be an object or array {{}}, []\n')
         
         result = self.parseValue()
 
@@ -47,11 +47,34 @@ class JSON_Parser:
         while self.index < len(self.jsonStr) and self.jsonStr[self.index] in WHITESPACE:
             self.index += 1
 
+    def process_colon(self):
+        if self.index < len(self.jsonStr) and self.jsonStr[self.index] == ":":
+            self.index += 1
+        else:
+            raise JSON_Exception(f"Colon expected after Key")
+
+    # Parses the value accordingly
     def parseValue(self):
-        result = self.parseObject()
+        result = self.parseString()
+        if result == None:
+            result = self.parseObject()
         
         return result
     
+    def parseString(self):
+        try:
+            if self.jsonStr[self.index] == '"':
+                result = ""
+                self.index += 1
+                self.skip_whitespace()
+                while self.jsonStr[self.index] != '"':
+                    result += self.jsonStr[self.index]
+                    self.index += 1
+                self.index += 1
+                return result
+        except Exception as e:
+            raise JSON_Exception(f'Failed while Parsing String: {e}')
+
 
     def parseObject(self):
         if self.jsonStr[self.index] == "{":
@@ -63,9 +86,30 @@ class JSON_Parser:
             
             try:
                 while self.jsonStr[self.index] != "}":
-                    break
+                    '''
+                    Add code for:
+                    if not opening brace:
+                        skip white
+                        colon
+                        skip white
+                    '''
+                    if not openingBrace:
+                        self.skip_whitespace()
+                        self.process_colon()
+                        self.skip_whitespace()
+
+                    key = self.parseString()
+                    # at or before colon
+                    self.skip_whitespace()
+                    self.process_colon()
+                    self.skip_whitespace()
+                    value = self.parseValue()
+                    result[key] = value
+                    self.index += 1
+                    openingBrace = False
+
             except Exception as e:
-                raise(f"{e}")
+                print(f"{e}")
 
             self.index += 1
             self.depth -= 1
@@ -74,4 +118,5 @@ class JSON_Parser:
 
 if __name__ == '__main__':
     jsonParser = JSON_Parser()
-        
+    test = '{"Key" :}'
+    result = jsonParser.jsonParse(test)
